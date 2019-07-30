e11abd2693bcce62388510ea9a84f90252ee805b
ctjcom <3264275716@qq.com>
2019-07-30 23:43:30 +0800

    1.3

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="login">
+  <div class="login" @click="hidelogin">
     <!--登录img-->
     <a href="javascript:;">
       <img class="top-img" src="../../public/img/top_red_13.png" alt />
@@ -13,15 +13,17 @@
       <div class="login-from">
         <ul class="from-list">
           <li>
-            <span>2</span>
-            <input type="text" placeholder="请输入邮箱/个性账号" />
+            <div>
+              <span class="iconfont icon-yonghu"></span>
+              <input type="text" placeholder="请输入邮箱/个性账号" />
+            </div>
           </li>
           <li>
             <div>
-              <span>2</span>
+              <span class="iconfont icon-suo"></span>
               <input type="password" placeholder="请输入密码" />
-              <span>2</span>
             </div>
+            <span class="iconfont icon-pwd-hidden"></span>
           </li>
         </ul>
       </div>
@@ -38,46 +40,46 @@
       <ul class="login-list">
         <li>
           <a href="#">
-            <p></p>
+            <p class="iconfont icon-qq" style="color:#0091de;backgroundColor:#92d7fc"></p>
             <span>QQ</span>
           </a>
         </li>
         <li>
           <a href="#">
-            <p></p>
+            <p class="iconfont icon-shouji"></p>
             <span>短信登录</span>
           </a>
         </li>
-        <li>
+        <li @click="showlogin">
           <a href="#">
-            <p></p>
+            <p class="iconfont icon-gengduo1"></p>
             <span>更多</span>
           </a>
         </li>
       </ul>
     </div>
     <!--更多的登录方式-->
-    <div class="login-methods">
-      <div class="toggle-btn">
+    <div class="login-methods" v-show="isShow">
+      <div class="toggle-btn" @click="isShow=false">
         <span>收起</span>
-        <em></em>
+        <em class="iconfont icon-jiankuohaoxixia" style="margin-left:0.3rem"></em>
       </div>
       <ul>
         <li>
           <a href="#">
-            <p></p>
+            <p class="iconfont icon-weibo" style="color:red"></p>
             <span>微博</span>
           </a>
         </li>
         <li>
           <a href="#">
-            <p></p>
+            <p class="iconfont icon-zhifubao" style="color:#92d7fc"></p>
             <span>支付宝</span>
           </a>
         </li>
         <li>
           <a href="#">
-            <p></p>
+            <p class="iconfont icon-icon_baidulogo" style="color:#1e35bb"></p>
             <span>百度</span>
           </a>
         </li>
@@ -87,15 +89,33 @@
 </template>
     
 <script>
-export default {};
+import { constants } from 'crypto';
+export default {
+  data() {
+    return {
+      isShow: false
+    }
+  },
+  methods: {
+    showlogin(){
+      this.isShow = true;
+    },
+    hidelogin(e){
+      if(e.target.nodeName=="DIV"){
+        this.isShow=false;
+      } 
+    }
+  },
+ 
+};
 </script>
 
 <style scoped>
 .login {
   font-family: "Helvetica Neue", Helvetica, STHeiTi, Arial, sans-serif;
-  background-color: #fff;
   color: #61708f;
   font-size: 0.7rem;
+  height: 100%;
 }
 .login .top-img {
   width: 100%;
@@ -115,7 +135,7 @@ export default {};
   height: 2.6rem;
   color: initial;
   line-height: 2.6rem;
-  width: 60%;
+  width: 12rem;
   background: none;
   font-size: 0.9rem;
 }
@@ -128,6 +148,12 @@ export default {};
   width: 100%;
   border-bottom: 1px solid #eee;
   margin-bottom: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.login .login-from .from-list li span{
+  margin-right: 0.9rem;
 }
 .login .login-link {
   display: flex;
@@ -160,36 +186,44 @@ export default {};
 }
 .login .login-method .login-list li p {
   /*图标样式*/
-  width: 3rem;
-  height: 3rem;
+  width: 2.5rem;
+  height: 2.5rem;
   margin: 0 auto 1rem;
   border-radius: 50%;
-  background: #ee434c;
+  font-size: 1.5rem;
+  line-height: 2.5rem;
+  background: #fdf6f6fa;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 5px 0 #eee;
 }
 .login .login-method .login-list li a {
   color: #61708f;
 }
 /*更多登录方式(默认隐藏)*/
-.login-methods{
-  box-shadow: 0 -2px 5px 0 #EEE;
+.login-methods {
+  box-shadow: 0 -2px 5px 0 #eee;
 }
-.login-methods .toggle-btn{
+.login-methods .toggle-btn {
   padding: 1rem 0 1.5rem 0;
 }
-.login-methods ul{
+.login-methods ul {
   display: flex;
   justify-content: space-between;
   padding: 0 5rem;
 }
-.login-methods ul li a{
-  color: #61708F;
+.login-methods ul li a {
+  color: #61708f;
 }
-.login-methods ul li p{
+.login-methods ul li p {
   /*图标样式*/
-  width: 3rem;
-  height: 3rem;
+  width: 2.5rem;
+  height: 2.5rem;
+  font-size: 1.5rem;
+  line-height: 2.5rem;
+  background: #fdf6f6;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 5px 0 #eee;
   margin: 0 auto 1rem;
   border-radius: 50%;
-  background: #ee434c;
 }
 </style>
